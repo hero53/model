4a2e867d0254524dc5c9e453ce10aaf71d3999b8
hero53 <45401008+hero53@users.noreply.github.com>
2022-04-05 18:57:36 +0000

    list de compo

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,55 @@ class MyApp extends StatelessWidget {
 }
 
 class Model extends StatelessWidget {
-  const Model({Key? key}) : super(key: key);
+  // const Model({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Welcome to Flutter'),
+        title: Text('Model'),
+        backgroundColor: Colors.orangeAccent,
       ),
-      body: const Center(
-        child: Text('kiny'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            titleSection,
+            titleSection,
+            titleSection,
+            titleSection,
+            titleSection,
+            titleSection,
+          ],
+        ),
       ),
     );
   }
+
+  Widget titleSection = Container(
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.all(15),
+    child: Row(children: [
+      Text(
+        "Title",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          color: Colors.red,
+        ),
+      ),
+      Container(
+        width: 70,
+        height: 70,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: NetworkImage(
+              'https://img.freepik.com/free-vector/royal-watercolor-valentine-red-background_1340-4006.jpg?t=st=1649175650~exp=1649176250~hmac=f54eeaaad25357a960c16183fb0d875cae8bc4838bb504a76119188eee083760&w=740',
+            ),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    ]),
+  );
 }
