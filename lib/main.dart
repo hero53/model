4d9de1ba79d52d39bd95db15632ea2802afb9d52
hero53 <45401008+hero53@users.noreply.github.com>
2022-04-28 20:11:29 +0000

    model de component

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Model(),
+      debugShowCheckedModeBanner: true,
     );
   }
 }
@@ -32,44 +33,133 @@ class Model extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [
-            titleSection,
-            titleSection,
-            titleSection,
-            titleSection,
-            titleSection,
-            titleSection,
-          ],
+          children: [bannerSection, titleSection, rowSection, contenerSection],
         ),
       ),
     );
   }
 
   Widget titleSection = Container(
-    padding: EdgeInsets.all(10),
-    margin: EdgeInsets.all(15),
-    child: Row(children: [
-      Text(
-        "Title",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-          color: Colors.red,
-        ),
-      ),
-      Container(
-        width: 70,
-        height: 70,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          image: DecorationImage(
-            image: NetworkImage(
-              'https://img.freepik.com/free-vector/royal-watercolor-valentine-red-background_1340-4006.jpg?t=st=1649175650~exp=1649176250~hmac=f54eeaaad25357a960c16183fb0d875cae8bc4838bb504a76119188eee083760&w=740',
+    // padding: EdgeInsets.all(10),
+    // margin: EdgeInsets.all(5),
+    child: Padding(
+      padding: const EdgeInsets.only(left: 15.0, top: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            "Title",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.red,
             ),
-            fit: BoxFit.fill,
+          ),
+          Container(
+            width: 125.0,
+            height: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('images/avatar.png'),
+                // fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+
+  Widget rowSection = Container(
+    color: Colors.black,
+    padding: EdgeInsets.all(20),
+    margin: EdgeInsets.all(20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          color: Colors.blue,
+          padding: EdgeInsets.all(20),
+        ),
+        Container(
+          color: Colors.orange,
+          padding: EdgeInsets.all(20),
+        ),
+        Container(
+          color: Colors.yellow,
+          padding: EdgeInsets.all(20),
+        ),
+        Container(
+          color: Colors.green,
+          padding: EdgeInsets.all(20),
+        ),
+        Container(
+          color: Colors.pink,
+          padding: EdgeInsets.all(20),
+        ),
+      ],
+    ),
+  );
+
+  Widget bannerSection = Container(
+    height: 200,
+    width: double.infinity,
+    padding: EdgeInsets.all(20),
+    margin: EdgeInsets.all(20),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.blue,
+          Colors.green,
+        ],
+      ),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Titre',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
           ),
         ),
+        SizedBox(height: 10),
+        Text("sous-titre"),
+        SizedBox(height: 10),
+        Text("long text par ici blalaallaallala"),
+        SizedBox(height: 10),
+        RaisedButton(
+          onPressed: () {},
+          color: Colors.orange,
+          child: Text(
+            "Acheter ici",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  Widget contenerSection = Container(
+    height: 200,
+    padding: EdgeInsets.all(20),
+    margin: EdgeInsets.all(20),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.blue,
+          Colors.yellow,
+        ],
       ),
-    ]),
+    ),
   );
 }
